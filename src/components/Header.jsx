e6a6091943de253a7b7d5b2efaad19728406dff6
wisuwat <wisuwatpond01@gmail.com>
2025-01-30 15:17:44 +0700

    Create Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header () {
+    return (
+        <section className="Header">
+            <h1>Assembly: Endgame</h1>
+            <p>Guess the word within 8 attempts to keep the programming
+                world safe from Assembly!
+            </p>
+        </section>
+    )
+}
