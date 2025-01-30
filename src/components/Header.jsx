76212aa4830472e4f5006ded7e4c740d1176a6b8
wisuwat <wisuwatpond01@gmail.com>
2025-01-30 16:03:23 +0700

    Add Word Guess

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react"
 import { languages } from "../../languages"
 
 export default function Header () {
+    const [currentWord, setCurrentWord] = useState("react")
+    
+    const word = currentWord.split("")
+
     return (
         <section className="Header">
             <h1>Assembly: Endgame</h1>
@@ -15,6 +20,13 @@ export default function Header () {
                 {
                     languages.map((l) => (
                         <span key={l.name} className="chip" style={{ backgroundColor: l.backgroundColor,  color: l.color}}>{l.name}</span>
+                    ))
+                }
+            </div>
+            <div className="word">
+                {
+                    word.map((w, index) => (
+                        <span key={index}>{w.toUpperCase()}</span>
                     ))
                 }
             </div>
