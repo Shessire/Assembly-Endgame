bbd975388db54fcef54734eb5338ad8665007c64
wisuwat <wisuwatpond01@gmail.com>
2025-02-11 13:46:00 +0700

    Add Render Status

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { languages } from "../../languages"
 export default function Header () {
     const [currentWord, setCurrentWord] = useState("react")
     const [guess, setGuess] = useState([])
-    
+
     const wrongGuessCount = guess.filter(letter => !currentWord.includes(letter)).length
     const isGameWon = currentWord.split("").every(letter => guess.includes(letter))
     const isGameLost = wrongGuessCount >= languages.length - 1
@@ -23,16 +23,36 @@ export default function Header () {
         ))
     }
 
+    const gameStatusClass = clsx("game-status", {
+        won: isGameWon,
+        lost: isGameLost
+    })
+
     return (
         <section className="Header">
             <h1>Assembly: Endgame</h1>
             <p>Guess the word within 8 attempts to keep the programming
                 world safe from Assembly!
             </p>
-            <div className="game-status">
-                <h2>You win!</h2>
-                <p>Well done! 🎉</p>
-            </div>
+            <section className={gameStatusClass}>
+            {
+                isGameOver ? (
+                    isGameWon ? (
+                        <>
+                            <h2>You win!</h2>
+                            <p>Well done! 🎉</p>
+                        </>
+                    ) : (
+                        <>
+                            <h2>Game over!</h2>
+                            <p>You lose! Better start learning Assembly 😈</p>
+                        </>
+                    )
+                ) : (
+                    null
+                )
+            }
+            </section>
             <div className="language-chips">
                 {
                     languages.map((l, index) => {
